d341ecad18032bdb94756a594aa1d763d698de44
Kate <nemochka99@gmail.com>
2020-02-19 20:09:21 +0200

    Updated tests.

--- src/ua/testing/test/ModelCompareNumberMethodTesting.java
+++ src/ua/testing/test/ModelCompareNumberMethodTesting.java
@@ -3,32 +3,31 @@ package ua.testing.test;
 import org.junit.Test;
 import ua.testing.main.Model;
 
-import static org.junit.Assert.assertEquals;
-/*
+import static org.junit.Assert.*;
+
 public class ModelCompareNumberMethodTesting {
     Model model = new Model();
     int secretNumber = model.getSecretNumber();
 
     @Test
     public void testSameToSecretResultAndBoundaries() {
-        assertEquals(0, model.compareNumber(secretNumber));
+        assertTrue(model.isSecret(secretNumber));
         assertEquals(model.RAND_MIN, model.getRangeMin());
         assertEquals(model.RAND_MAX, model.getRangeMax());
     }
 
     @Test
     public void testBiggerThanSecretResultAndBoundaries() {
-        assertEquals(1, model.compareNumber(89));
+        assertTrue(!model.isSecret(secretNumber + 10));
         assertEquals(model.RAND_MIN, model.getRangeMin());
-        assertEquals(89, model.getRangeMax());
+        assertEquals(secretNumber + 10, model.getRangeMax());
     }
 
     @Test
     public void testSmallerThanSecretResultAndBoundaries() {
-        assertEquals(-1, model.compareNumber(secretNumber-10));
-        assertEquals(secretNumber-10, model.getRangeMin());
+        assertTrue(!model.isSecret(secretNumber - 10));
+        assertEquals(secretNumber - 10, model.getRangeMin());
         assertEquals(model.RAND_MAX, model.getRangeMax());
     }
 
 }
-*/
